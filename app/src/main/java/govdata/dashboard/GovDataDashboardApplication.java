e4fe3977dfaaa36bb78257ead905be550bf405a0
Benjamin Kodera <48090308+bkodera@users.noreply.github.com>
2024-10-02 20:39:38 +0200

    Create GovDataDashboardApplication entry point

---
+++ app/src/main/java/govdata/dashboard/GovDataDashboardApplication.java
@@ -0,0 +1,12 @@
+package govdata.dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GovDataDashboardApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(GovDataDashboardApplication.class, args);
+  }
+}
