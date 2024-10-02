3d92e2f5e83612453b29914661ffbff9749615a6
Benjamin Kodera <48090308+bkodera@users.noreply.github.com>
2024-10-02 21:03:59 +0200

    Implement controller classes for accessing the data set counts via GET requests
    - one controller for viewing the result as raw json
    - one controller for rendering the result as html table (with error handling)

---
+++ app/src/main/java/govdata/dashboard/controller/DashboardRenderingController.java
@@ -0,0 +1,34 @@
+package govdata.dashboard.controller;
+
+import govdata.dashboard.service.DashboardService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.reactive.result.view.Rendering;
+import reactor.core.publisher.Mono;
+
+@Controller
+@RequestMapping("/api/v0.1/dashboard")
+@RequiredArgsConstructor
+public class DashboardRenderingController {
+
+  private final DashboardService dashboardService;
+
+  @GetMapping("/")
+  public Mono<Rendering> renderAllDataSetsByFederalMinistry() {
+    return this.dashboardService.computeDepartmentDataSetCounts()
+      .collectList()
+      .map(datasets ->
+        Rendering.view("index").modelAttribute("datasets", datasets).build()
+      )
+      .onErrorResume(error ->
+        Mono.just(
+          Rendering
+            .view("error")
+            .modelAttribute("errorMessage", error.getMessage())
+            .build()
+        )
+      );
+  }
+}
---
+++ app/src/main/java/govdata/dashboard/controller/DashboardRestController.java
@@ -0,0 +1,25 @@
+package govdata.dashboard.controller;
+
+import govdata.dashboard.model.DepartmentDto;
+import govdata.dashboard.service.DashboardService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+@RestController
+@RequestMapping("/api/v0.1/dashboard")
+@RequiredArgsConstructor
+public class DashboardRestController {
+
+  private final DashboardService dashboardService;
+
+  @GetMapping("/json")
+  public ResponseEntity<Flux<DepartmentDto>> getAllDataSetsByFederalMinistry() {
+    return ResponseEntity.ok(
+      this.dashboardService.computeDepartmentDataSetCounts()
+    );
+  }
+}
